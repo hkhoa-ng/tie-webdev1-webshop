080781c75db3680d076ffcaf0d17b5a504e28d8f
Ahmad Sharif <ahmad.sharif@tuni.fi>
2022-04-01 21:28:08 +0300

    Refactoring

--- routes.js
+++ routes.js
@@ -108,64 +108,18 @@ const handleRequest = async (request, response) => {
   }
 
   if (matchUserId(filePath)) {
-    // DONE: 8.6 Implement view, update and delete a single user by ID (GET, PUT, DELETE)
-    if (!request.headers.authorization) {
-      response.setHeader("WWW-Authenticate", "Basic");
-      return responseUtils.unauthorized(response);
-    }
+    UserController.viewUser(request, response)
+  }
 
-    const authorizationHeader = headers["authorization"];
-    if (!authorizationHeader) {
-      // response with basic auth challenge if auth header is missing/empty
-      return responseUtils.basicAuthChallenge(response);
-    }
 
-    if (method.toUpperCase() === "OPTIONS") {
-      return sendOptions(filePath, response);
-    }
-    const currentUser = await getCurrentUser(request);
-    if (currentUser === null) {
-      // response with basic auth challenge if credentials are incorrect
-      return responseUtils.basicAuthChallenge(response);
-    }
-    const uid = filePath.split("/")[3];
-    if (User.findById(uid).exec() === null) {
-      return responseUtils.notFound(response);
-    }
-    // response with basic auth challenge if customer credentials are parse
-    if (currentUser.role === "customer") {
-      return responseUtils.forbidden(response);
-    }
-    if (method.toUpperCase() === "GET" && currentUser.role === "admin") {
-      return responseUtils.sendJson(response, currentUser);
-    }
-    if (method.toUpperCase() === "DELETE" && currentUser.role === "admin") {
-      // Find user to be deleted with ID, and then delete and return that user
-      const userToBeDeleted = await User.findById(uid).exec();
-      if (userToBeDeleted === null) {
-        return responseUtils.notFound(response);
-      }
-      await User.deleteOne({ _id: uid });
-      return responseUtils.sendJson(response, userToBeDeleted);
-    }
-    const userChangeRole = await parseBodyJson(request);
-    if (method.toUpperCase() === "PUT") {
-      const roleToChange = userChangeRole.role;
-      if (
-        (roleToChange !== "admin" && roleToChange !== "customer") ||
-        !roleToChange
-      ) {
-        return responseUtils.badRequest(response);
-      }
+  if (method.toUpperCase() === "PUT") {
+    UserController.updateUser(request, response)
+            
+  }
 
-      if (currentUser.role === "admin") {
-        // Only update user role if role = admin
-        const userToChange = await User.findById(uid).exec();
-        userToChange.role = roleToChange;
-        await userToChange.save();
-        return responseUtils.sendJson(response, userToChange);
-      }
-    }
+  if (method.toUpperCase() === "DELETE" && currentUser.role === "admin") {
+    UserController.deleteUser(request, response)
+           
   }
 
   // Default to 404 Not Found if unknown url
@@ -187,80 +141,17 @@ const handleRequest = async (request, response) => {
 
   // GET all users
   if (filePath === "/api/users" && method.toUpperCase() === "GET") {
-    // DONE: 8.5 Add authentication (only allowed to users with role "admin")
-    const authorizationHeader = headers["authorization"];
-    if (!authorizationHeader) {
-      // response with basic auth challenge if auth header is missing/empty
-      return responseUtils.basicAuthChallenge(response);
-    }
+    UserController.getAllUsers(request, response)
 
-    // check if the header is properly encoded
-    const credentials = authorizationHeader.split(" ")[1];
-    const base64regex =
-      /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
-    if (!base64regex.test(credentials)) {
-      // response with basic auth challenge if auth header is not properly encoded
-      return responseUtils.basicAuthChallenge(response);
-    }
-
-
-    getCurrentUser(request).then((currentUser) => {
-      if (currentUser === null) {
-        // response with basic auth challenge if credentials are incorrect (no user found)
-        return responseUtils.basicAuthChallenge(response);
-      }
-      // response with basic auth challenge if customer credentials are parsed
-      if (currentUser.role === "customer") {
-        return responseUtils.forbidden(response);
-      }
-      if (currentUser.role === "admin") {
-        // respond with json when admin credentials are received
-        return responseUtils.sendJson(response, users);
-      }
-    });
-    
   }
 
   // register new user
   if (filePath === "/api/register" && method.toUpperCase() === "POST") {
-    // DONE: 8.4 Implement user registration
-    // Fail if not a JSON request, don't allow non-JSON Content-Type
-    if (!isJson(request)) {
-      return responseUtils.badRequest(
-        response,
-        "Invalid Content-Type. Expected application/json"
-      );
-    }
-    const userAsJson = await parseBodyJson(request);
-    const user = await User.findOne({ email: userAsJson.email }).exec();
-    const errors = validateUser(userAsJson);
-    if (errors.length) {
-      return responseUtils.badRequest(response, errors);
-    }
-    if (user) {
-      return responseUtils.badRequest(response, "Email already in use!");
-    }
-    // Create a new user
-    const userData = {
-      name: userAsJson.name,
-      email: userAsJson.email,
-      password: userAsJson.password,
-      role: "customer",
-    }; 
-    const newUser = new User(userData);
-    await newUser.save();
-    const newId = newUser._id;
-
-    const newlyAddedUser = await User.findOne({ _id: newId }).exec();
-    newlyAddedUser.role = "customer";
-    await newlyAddedUser.save();
-    return responseUtils.createdResource(response, newUser);
+    UserController.registerUser(request, response)
+    
   }
 
   if (filePath === "/api/products" && method.toUpperCase() === "GET") {
-    /**
-     * AUTHORIZATION: Chech the authorization header of the request and response accordingly
-     */
      ProductController.getAllProducts(request, response)
   
   }
