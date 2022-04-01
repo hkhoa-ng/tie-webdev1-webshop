9e4643a8e862aa7adfdae7c7403d79cb66dd1bec
Ahmad Sharif <ahmad.sharif@tuni.fi>
2022-04-01 11:19:31 +0300

    10

---
+++ test/own/issue.test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+
+
+ describe('Test issues', () => {
+      it('Test class', async () => {
+
+           expect(function(){utils.which_min();}).to.throw("Cannot read property 'length' of undefined")
+
+        
+      });
+
+      it('Issue 01', async () => {
+        
+      });
+
+       it('Issue 02', async () => {
+        
+      });
+
+
+      it('Issue 03', async () => {
+        
+      });
+
+      
+});
